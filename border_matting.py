a97c43fa6fd94336c29702e970b6aa3ead0dcee5
徐近庭 <xujinting@xujintingdeMacBook-Pro.local>
2023-05-10 17:18:26 +0800

    start work on

---
+++ border_matting.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+'''
+Implementation of the border matting algorithm based on paper:
+Available at: https://dl.acm.org/doi/10.1145/1186562.1015720?fbclid=IwAR2gI_CV9mQmJD8lLp_DoHJw9Zl8QdKWxUp_J7CFLr_god47yYVqp1w_-TE
+
+This is the term project of digital image processing course in NTU.
+This code do border matting on a given image and trimap.
+
+'''
+
